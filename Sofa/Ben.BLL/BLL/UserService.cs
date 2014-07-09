5d74783cbdb9c94d5f35f4233ef8c896789b8ee2
Daive <243627986@qq.com>
2014-07-09 17:44:48 +0800

    sd

---
+++ Sofa/Ben.BLL/BLL/UserService.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using Ben.BLL.IBLL;
+using Ben.DAL.DAL;
+using Ben.DAL.IDAL;
+using Ben.Entity;
+using Microsoft.AspNet.Identity;
+
+
+namespace Ben.BLL.BLL
+{
+    public class UserService : IUserService
+    {
+        public readonly IBaseRepository<User> _userRepository;
+
+        public UserService()
+        {
+            _userRepository = new BaseRepository<User>();
+        }
+
+        public bool Exist(string userName)
+        {
+            return _userRepository.Exist(u => u.UserName == userName);
+        }
+
+        public User Add(User user)
+        {
+            return _userRepository.Add(user);
+        }
+
+        public ClaimsIdentity CreateiIdentity(User user, string authenticationType)
+        {
+            var _identity = new ClaimsIdentity(DefaultAuthenticationTypes.ApplicationCookie);
+            _identity.AddClaim(new Claim(ClaimTypes.Name,user.UserName));
+            _identity.AddClaim(new Claim(ClaimTypes.NameIdentifier,user.UserID.ToString()));
+            _identity.AddClaim(new Claim("http://schemas.microsoft.com/accesscontrolservice/2010/07/claims/identityprovider", "ASP.NET Identity"));
+            _identity.AddClaim(new Claim("DisplayName",user.DisplayName));
+            return _identity;
+        }
+    }
+}
---
+++ Sofa/Ben.BLL/IBLL/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Ben.Entity;
+
+namespace Ben.BLL.IBLL
+{
+    public interface IUserService
+    {
+        bool Exist(string userName);
+        User Add(User user);
+        ClaimsIdentity CreateiIdentity(User user, string authenticationType);
+
+
+    }
+}
